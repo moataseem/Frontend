861bdfd86d0351085a7c1cddc65144973a7f332e
Moataseem Bakri <Moataseem.Bakri@wolterskluwer.com>
2025-01-07 13:43:45 +0100

    react - fix

--- vite-react/src/main.tsx
+++ vite-react/src/main.tsx
@@ -1,16 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import whyDidYouRender from '@welldone-software/why-did-you-render';
-import React, { StrictMode } from 'react';
-if (process.env.NODE_ENV === 'development') {
-  whyDidYouRender(React, {
-    trackAllPureComponents: true,
-  });
-}
+import { StrictMode } from 'react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <App />
+    <App />
   </StrictMode>,
 )
